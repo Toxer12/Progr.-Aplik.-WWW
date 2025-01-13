bfbe536d0c478eec8f6e9a379af85708b531ed9b
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2025-01-13 18:52:31 +0100

    Update cfg.php

--- ProjAppwww lab1.5/cfg.php
+++ ProjAppwww lab1.5/cfg.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Domyślny użytkownik MySQL w XAMPP
-$password = ""; // Puste hasło dla domyślnego użytkownika w XAMPP
+$username = "root";
+$password = "";
 $dbname = "moja_strona";
 
-// Połączenie z bazą danych
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Sprawdzenie połączenia
 if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
